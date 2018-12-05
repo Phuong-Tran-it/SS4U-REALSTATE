9f3ebb8c6088fd9be3d2a9a6f92d9f6c7e96e413
phuongphonui98 <40985886+phuongphonui98@users.noreply.github.com>
2018-12-05 15:35:03 +0700

    lần 1.1

--- home.php
+++ home.php
@@ -1,8 +1,3 @@
-<!--Template Name: UrbanArchi
-File Name: home.html
-Author Name: ThemeVault
-Author URI: http://www.themevault.net/
-License URI: http://www.themevault.net/license/-->
 
 <!DOCTYPE html>
 <html lang="en">
@@ -196,7 +191,74 @@ License URI: http://www.themevault.net/license/-->
                     <div class="finder-block-inner">
                         <form>
                             <div class="col-md-3 col-sm-6 col-xs-12 scrollReveal sr-scaleDown sr-delay-1">
-                                <select><option>Vị Trí</option></select>
+                                <select>
+									<option value="-1">--Tỉnh/Thành phố--</option>
+									<option value="43">Hà Nội</option>
+									<option value="44">Hồ Chí Minh</option>
+									<option value="31">An Giang</option>
+									<option value="37">Bà Rịa Vũng Tàu</option>
+									<option value="33">Bình Dương</option>
+									<option value="35">Bình Phước</option>
+									<option value="28">Bình Thuận</option>
+									<option value="32">Bình Định</option>
+									<option value="34">Bạc Liêu</option>
+									<option value="6">Bắc Cạn</option>
+									<option value="12">Bắc Giang</option>
+									<option value="13">Bắc Ninh</option>
+									<option value="36">Bến Tre</option>
+									<option value="2">Cao Bằng</option>
+									<option value="38">Cà Mau</option>
+									<option value="30">Cần Thơ</option>
+									<option value="40">Đà Nẵng</option>
+									<option value="39">Đăk Lăk</option>
+									<option value="64">Điện Biên</option>
+									<option value="29">Đồng Nai</option>
+									<option value="41">Đồng Tháp</option>
+									<option value="42">Gia Lai</option>
+									<option value="1">Hà Giang</option>
+									<option value="18">Hà Nam</option>
+									<option value="14">Hà Tây</option>
+									<option value="46">Hà Tĩnh</option>
+									<option value="62">Hà Đông</option>
+									<option value="17">Hòa Bình</option>
+									<option value="16">Hưng Yên</option>
+									<option value="63">Hạ Long</option>
+									<option value="15">Hải Dương</option>
+									<option value="45">Hải Phòng</option>
+									<option value="48">Khánh Hòa</option>
+									<option value="47">Kiên Giang</option>
+									<option value="49">KonTum</option>
+									<option value="3">Lai Châu</option>
+									<option value="50">Long An</option>
+									<option value="4">Lào Cai</option>
+									<option value="51">Lâm Đồng</option>
+									<option value="52">Lạng Sơn</option>
+									<option value="19">Nam Định</option>
+									<option value="53">Nghệ An</option>
+									<option value="21">Ninh Bình</option>
+									<option value="54">Ninh Thuận</option>
+									<option value="10">Phú Thọ</option>
+									<option value="26">Phú Yên</option>
+									<option value="22">Quảng Bình</option>
+									<option value="55">Quảng Nam</option>
+									<option value="25">Quảng Ngãi</option>
+									<option value="56">Quảng Ninh</option>
+									<option value="23">Quảng Trị</option>
+									<option value="57">Sóc Trăng</option>
+									<option value="9">Sơn La</option>
+									<option value="59">Thanh Hóa</option>
+									<option value="20">Thái Bình</option>
+									<option value="7">Thái Nguyên</option>
+									<option value="24">Thừa Thiên Huế</option>
+									<option value="58">Tiền Giang</option>
+									<option value="60">Trà Vinh</option>
+									<option value="5">Tuyên Quang</option>
+									<option value="27">Tây Ninh</option>
+									<option value="61">Vĩnh Long</option>
+									<option value="11">Vĩnh Phúc</option>
+									<option value="8">Yên Bái</option>
+									<option value="65">Nơi khác</option>
+								</select>
                             </div>
                             <div class="col-md-3 col-sm-6 col-xs-12 scrollReveal sr-scaleDown sr-delay-2">
                                 <select><option>Thể Loại</option></select>
